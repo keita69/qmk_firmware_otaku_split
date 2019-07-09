da41f213c86b4a0c2db6cd9f841b8c29abde1768
keita69sawada <keita69sawada@gmail.com>
2019-07-10 05:57:11 +0900

    update

--- keyboards/otaku_split/rev1/keymaps/default/keymap.c
+++ keyboards/otaku_split/rev1/keymaps/default/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  ||||||||||||||||||||||AltSpc|   N  |   M  |   ,  |   .  |   /  | ---- | Shift|
      * |-------------+------+------+------+------+------+------+------+------|------+------+------+------+------+-------------|
-     * | Ctrl | GUI  | Alt  | Space| EISU | Lower| Space||||||||||||||| Bksp | Raise|AltTab| GUI  |GUI_UP|GUI_DW|CLATDL| Ctrl |
+     * | Ctrl | GUI  | Alt  | Space| EISU | Lower|   -  ||||||||||||||| Bksp | Raise|AltTab| GUI  |GUI_UP|GUI_DW|CLATDL| Ctrl |
      * ,----------------------------------------------------------------------------------------------------------------------.
      */
   [_QWERTY] = LAYOUT(
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_DEL ,      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC , KC_ENT ,
 			KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,               KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT  ,
 			KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,               ALT_SP , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______ , KC_RSFT,
-			KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , EISU   , LOWER  , KC_SPC ,      KC_BSPC, RAISE  , ALT_TB , KC_RGUI, GUI_RT , GUI_UP , CLALDL  , KC_RCTL
+			KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , EISU   , LOWER  , KC_MINS,      KC_BSPC, RAISE  , ALT_TB , KC_RGUI, GUI_RT , GUI_UP , CLALDL  , KC_RCTL
   ),
      /* Lower
      * ,----------------------------------------------------------------------------------------------------------------------.
@@ -200,8 +200,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         // http://okapies.hateblo.jp/entry/2019/02/02/133953
         if (lower_pressed) {
-          register_code(KC_MINS);
-          unregister_code(KC_MINS);
+          register_code(KC_SPC);
+          unregister_code(KC_SPC);
         }
         lower_pressed = true;
       }
