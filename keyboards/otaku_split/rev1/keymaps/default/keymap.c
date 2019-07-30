e5a65de04169f4b8d1c140229749b8a7922f1119
keita69sawada <keita69sawada@gmail.com>
2019-07-30 21:56:05 +0900

    keymap update

--- keyboards/otaku_split/rev1/keymaps/default/keymap.c
+++ keyboards/otaku_split/rev1/keymaps/default/keymap.c
@@ -90,17 +90,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+--------------+------+------+------+------+------+------+------+      |
      * | Ctrl |   A  |   S  |   D  |   F  |   G  |||||||||||||||||||||||   H  |   J  |   K  |   L  |   ;  |   ;  | Enter|      |
      * |------+------+------+------+------+------+---------------------+------+------+------+------+------+------+------+------|
-     * | Shift|   Z  |   X  |   C  |   V  |   B  |||||||||||||||||||||||AltSpc|   N  |   M  |   ,  |   .  |   /  | ---- | Shift|
+     * | Shift|   Z  |   X  |   C  |   V  |AltSpc|||||||||||||||||||||||   B  |   N  |   M  |   ,  |   .  |   /  | ---- | Shift|
      * |-------------+------+------+------+------+------+------+-------+------|------+------+------+------+------+-------------|
-     * | Ctrl | GUI  | Alt  | Space| EISU | Lower| Del  |||||||||||||||| Bksp | Raise|AltTab| GUI  |GUI_UP|GUI_DW|CLATDL| Ctrl |
+     * | Ctrl | GUI  | Alt  | Space|AltTab| Lower| Del  |||||||||||||||| Bksp | Raise| EISU | GUI  |GUI_UP|GUI_DW|CLATDL| Ctrl |
      * ,----------------------------------------------------------------------------------------------------------------------.
      */
   [_QWERTY] = LAYOUT(
 			KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,      SD_GRV3, KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL  , KC_BSLS, KC_GRV,
 			KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , K8SCTL ,      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC , KC_ENT ,
 			KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,               KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT  ,
-			KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,               ALT_SP , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______ , KC_RSFT,
-			KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , EISU   , LOWER  , KC_DEL ,      KC_BSPC, RAISE  , ALT_TB , KC_RGUI, GUI_RT , GUI_UP , CLALDL  , KC_RCTL
+			KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , ALT_SP ,               KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______ , KC_RSFT,
+			KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , ALT_TB , LOWER  , KC_DEL ,      KC_BSPC, RAISE  , EISU   , KC_RGUI, GUI_RT , GUI_UP , CLALDL  , KC_RCTL
   ),
      /* Lower
      * ,----------------------------------------------------------------------------------------------------------------------.
